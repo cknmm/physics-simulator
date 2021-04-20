e86675506fa5ae8722b151a61d18244a69b57006
cknmm <76263657+cknmm@users.noreply.github.com>
2021-04-21 04:41:13 +0530

    Added fairly accurate collisions, new kinematics

--- physicsenginedemo.py
+++ physicsenginedemo.py
@@ -1,4 +1,3 @@
-
 import pygame
 from pygame.locals import *
 import threading as t
@@ -42,75 +41,79 @@ class Vector2:
         return Vector2(-self.x, -self.y)
         
 class CollisionHandler:
-	
-	def __init__(self, collision_list=[]):
-		
-		self.collision_list = collision_list
-		pairs = []
-		
-		self.assign_pairs()
-		
-		t.Thread(target=self.update_collisions).start()
-		
-	def assign_pairs(self):
-		
-		assigned_pairs = []
-		pairs = []
-		for i in self.collision_list:
-			for j in self.collision_list:
-				if i != j:
-					pairs.append([i, j])
-		#removing duplicates
-		to_remove = []
-		for i in pairs:
-			for j in pairs:
-				if i != j:
-					if j[0] in i and j[1] in i:
-						if i not in to_remove:
-							to_remove.append(j)
-							break
-						else:
-							break
-		for i in to_remove:
-			pairs.remove(i)
-		
-		self.unique_pairs = list(pairs)
-		
-	def update_collisions(self):
-		
-		global INITIATEEXIT
-		
-		lct = {}
-		for i in self.collision_list:
-			lct[i] = 0
-		
-		while not(INITIATEEXIT):
-			
-			s = time.time()
-			
-			for i in self.unique_pairs:
-				p1 = i[0]
-				p2 = i[1]
-				
-				r1 = p1.get_rect()
-				r2 = p2.get_rect()
-				
-				if r1.colliderect(r2):
-					if p1.collision_data != id(p2) or lct[p1] >= 0.2:
-						p1.net_force, p2.net_force = p2.net_force, p1.net_force
-						p1.collision_data = id(p2)
-						p2.collision_data = id(p1)
-						lct[p1] = 0
-						lct[p2] = 0
-					#p1.net_force, p2.net_force = p2.net_force, p1.net_force
-			
-			for i in lct:
-				lct[i] += 1/120
-				
-			e = time.time()
-			
-			if e-s < 1/120:
-				time.sleep(1/120 - (e-s))
+    
+    def __init__(self, collision_list=[]):
+        
+        self.collision_list = collision_list
+        pairs = []
+        
+        self.assign_pairs()
+        
+        t.Thread(target=self.update_collisions).start()
+        
+    def assign_pairs(self):
+        
+        assigned_pairs = []
+        pairs = []
+        for i in self.collision_list:
+            for j in self.collision_list:
+                if i != j:
+                    pairs.append([i, j])
+        #removing duplicates
+        to_remove = []
+        for i in pairs:
+            for j in pairs:
+                if i != j:
+                    if j[0] in i and j[1] in i:
+                        if i not in to_remove:
+                            to_remove.append(j)
+                            break
+                        else:
+                            break
+        for i in to_remove:
+            pairs.remove(i)
+        
+        self.unique_pairs = list(pairs)
+        
+    def update_collisions(self):
+        
+        global INITIATEEXIT
+        
+        lct = {}
+        for i in self.collision_list:
+            lct[i] = 0
+        
+        while not(INITIATEEXIT):
+            
+            s = time.time()
+            
+            for i in self.unique_pairs:
+                p1 = i[0]
+                p2 = i[1]
+                
+                r1 = p1.get_rect()
+                r2 = p2.get_rect()
+                
+                if r1.colliderect(r2):
+                    if p1.collision_data != id(p2) or lct[p1] >= 0.2:
+                        #velocity exchange as per law of conservation of momentum
+                        total_mass = p1.mass + p2.mass
+                        v1 = p1.velocity*((p1.mass - p2.mass)/total_mass) + p2.velocity*(2*p2.mass/total_mass)
+                        v2 = p2.velocity*((p2.mass - p1.mass)/total_mass) + p1.velocity*(2*p1.mass/total_mass)
+                        p1.velocity, p2.velocity = v1*0.9, v2*0.9
+                        
+                        p1.collision_data = id(p2)
+                        p2.collision_data = id(p1)
+                        lct[p1] = 0
+                        lct[p2] = 0
+            
+            for i in lct:
+                lct[i] += 1/120
+                
+            e = time.time()
+            
+            if e-s < 1/120:
+                time.sleep(1/120 - (e-s))
 
 class PhysicsParticle:
 
@@ -129,6 +132,7 @@ class PhysicsParticle:
 
         self.net_force = Vector2()
         self.velocity = Vector2()
+        self.acceleration = Vector2()
         self.collision_data = 0
         
         t.Thread(target=self.update_physics).start()
@@ -153,8 +157,13 @@ class PhysicsParticle:
         if "collision_list" not in keys:
             self.kwargs["collision_list"] = []
 
-    def apply_force(self, force=Vector2()):
+    def apply_force(self, force=Vector2(), act_time=0.1):
         self.net_force += force
+        time.sleep(act_time)
+        self.net_force -= force
+        
+    def apply_impulse(self, force=Vector2(), for_time=0.1):
+        t.Thread(target=self.apply_force, args=(force, for_time)).start()
 
     def get_rect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
@@ -183,33 +192,37 @@ class PhysicsParticle:
             if self.is_pawn:
                 key_events = pygame.key.get_pressed()
                 if key_events[pygame.K_w]:
-                    self.apply_force(Vector2(0, -0.2))
+                    self.apply_impulse(Vector2(0, -0.2), 1)
                 if key_events[pygame.K_s]:
-                    self.apply_force(Vector2(0, 0.2))
+                    self.apply_impulse(Vector2(0, 0.2), 1)
                 if key_events[pygame.K_a]:
-                    self.apply_force(Vector2(-0.2, 0))
+                    self.apply_impulse(Vector2(-0.2, 0), 1)
                 if key_events[pygame.K_d]:
-                    self.apply_force(Vector2(0.2, 0))
+                    self.apply_impulse(Vector2(0.2, 0), 1)
                 delta_time_input += 1/60
-            if self.net_force:
-                x_component = self.net_force.x
-                y_component = self.net_force.y
-                if x_component:
-                    if previous_force.x != x_component:
-                        velocity_x = x_component/self.mass
-                        inertial_x = velocity_x
-                        self.x += velocity_x
-                    else:
-                        self.x += inertial_x
-                if y_component:
-                    if previous_force.y != y_component:
-                        velocity_y = y_component/self.mass
-                        inertial_y = velocity_y
-                        self.y += velocity_y
-                    else:
-                        self.y += inertial_y
+                
+            self.acceleration = self.net_force*(1/self.mass)
+                
+            if self.net_force:                
+                
+                if self.acceleration.x:
+                    velocity_x = self.acceleration.x*(1/60)
+                    
+                if self.net_force.y:
+                    velocity_y = self.acceleration.y*(1/60)
+                    
+            else:
+                velocity_x, velocity_y = 0, 0
+                
+            if self.velocity:
+                
+                if self.velocity.x:
+                    self.x += self.velocity.x
+                    
+                if self.velocity.y:
+                    self.y += self.velocity.y
                         
-            self.velocity = Vector2(velocity_x, velocity_y)
+            self.velocity += Vector2(velocity_x, velocity_y)
             ends_at = time.time()
             net_time_taken = ends_at - starts_at
             if net_time_taken < 1/60:
@@ -224,7 +237,7 @@ class PhysicsParticle:
 main = pygame.display.set_mode((600, 600))
 
 physics_objects = [PhysicsParticle(pawn=True, pos=(500, 100), color=(255, 0, 0))]
-for i in range(30):
+for i in range(25):
     physics_objects.append(PhysicsParticle(pos=(i + i*20, 100), mass=randint(3, 5)))
 for i in physics_objects:
     i.collision_list = physics_objects
@@ -245,7 +258,7 @@ while True:
     if keys[pygame.K_r] or pygame.mouse.get_pressed()[0]:
         for i in physics_objects:
             x, y = randint(-1, 1), randint(-1, 1)
-            i.apply_force(Vector2(x, y))
+            i.apply_impulse(Vector2(x, y), 1)
 
     main.fill((0, 0, 0))
 
